976ab10b8d32de49054ba860f34d67bd44d30bbb
Tam <hi@tam.sx>
2018-04-26 18:24:17 +0100

    Tweaked date input font size

--- resources/ui/src/components/form/DateTime.vue
+++ resources/ui/src/components/form/DateTime.vue
@@ -265,7 +265,7 @@
 			width: 100%;
 			padding: 7px 15px;
 
-			font-size: 16px;
+			font-size: 14px;
 
 			cursor: pointer;
 			appearance: none;
--- src/web/assets/ui/dist/assets-manifest.php
+++ src/web/assets/ui/dist/assets-manifest.php
@@ -6,9 +6,9 @@
 class WebpackBuiltFiles {
   static $jsFiles = [
     'app' => 'js/app.bd4d30ef.js',
-    'vendor' => 'js/vendor.b483640b.js',
+    'vendor' => 'js/vendor.1ef455c5.js',
   ];
   static $cssFiles = [
-    'app' => 'css/app.b4578f20.css',
+    'app' => 'css/app.76acd935.css',
   ];
 }
